1b61bf24423ef26c929c19bd2f05205c417acd82
naveenautomationlabs <naveenautomationlabs@naveenautomationlabss-MacBook-Pro.local>
2020-10-26 07:48:21 +0530

    made chnages in DP class

--- src/main/java/com/qa/hubspot/pages/DealsPage.java
+++ src/main/java/com/qa/hubspot/pages/DealsPage.java
@@ -1,15 +1,23 @@
 package com.qa.hubspot.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 import com.qa.hubspot.base.BasePage;
 
-public class DealsPage extends BasePage{
+public class DealsPage extends BasePage {
 
-	public DealsPage() {
+	WebDriver driver;
+
+	By deals = By.id("deals");
+
+	public DealsPage(WebDriver driver) {
 		System.out.println("deals page");
+		driver = this.driver;
 	}
-	
-	
-	
-	
-	
+
+	public String getTitle() {
+		return driver.getTitle();
+	}
+
 }
