3c978af05cdf5a74b2678ddff7ce79c9165b1aa5
Naveen AutomationLabs <naveenanimation20@gmail.com>
2020-10-26 08:04:18 +0530

    Create ProfilePage.java

---
+++ src/main/java/com/qa/hubspot/pages/ProfilePage.java
@@ -0,0 +1,24 @@
+  
+package com.qa.hubspot.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.qa.hubspot.base.BasePage;
+
+public class ProfilePage extends BasePage {
+
+	WebDriver driver;
+
+	By deals = By.id("profile");
+
+	public ProfilePage(WebDriver driver) {
+		System.out.println("profile page");
+		driver = this.driver;
+	}
+
+	public String getTitle() {
+		return driver.getTitle();
+	}
+
+}
